837b0fd5baff93e4598ded2a572f5eaf0e9cff4a
Bilalusuf93 <42065447+Bilalusuf93@users.noreply.github.com>
2024-04-28 17:23:23 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { merge2 } from "./Algos/MergeSortedArray";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  React.useEffect(() => {
+    merge2([0], 0, [1], 1);
+    // merge2([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
